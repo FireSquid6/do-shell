097d018f19f94f3b7ba22e0e41cbab3a1e55218a
firesquid6 <jdeiss06@gmail.com>
2024-06-03 10:02:13 -0500

    add more to the parser

--- parser/parser.go
+++ parser/parser.go
@@ -1,1 +1,13 @@
 package parser
+
+import (
+	"github.com/firesquid6/do-shell/lexer"
+	"github.com/firesquid6/do-shell/token"
+	"github.com/firesquid6/do-shell/tree"
+)
+
+type Parser struct {
+	l     *lexer.Lexer
+	token token.Token
+}
+
--- tree/tree.go
+++ tree/tree.go
@@ -1,30 +1,43 @@
 package tree
 
+import "github.com/firesquid6/do-shell/token"
 
 type Node interface {
-  TokenLiteral() string
+	TokenLiteral() []rune
 }
 
 type Statement interface {
-  Node
-  statementNode()  // dummy method to help the compiler
+	Node
+	statementNode() // dummy method to help the compiler
 }
 
 type Expression interface {
-  Node
-  expressionNode()  // dummy method to help the compiler
+	Node
+	expressionNode() // dummy method to help the compiler
 }
-
 
 type Program struct {
-  Statements []Statement
+	Statements []Statement
 }
 
-
-func (p *Program) TokenLiteral() string {
-  if len(p.Statements) > 0 {
-    return p.Statements[0].TokenLiteral()
-  } else {
-    return ""
-  }
+type Identifier struct {
+	Token token.Token
+	Value string
 }
+
+func (p *Program) TokenLiteral() []rune {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return []rune{}
+	}
+}
+
+type LetStatement struct {
+	Token      token.Token
+	Name       *Identifier
+	Expression Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() []rune { return ls.Token.Literal }
