53f0a69236c47bcb0c2a61a684b61ef552300f1d
firesquid6 <jdeiss06@gmail.com>
2024-06-05 15:56:06 -0500

    add better stuff

--- parser/parser.go
+++ parser/parser.go
@@ -5,11 +5,38 @@ import (
 	"github.com/firesquid6/do-shell/tree"
 )
 
+type (
+	prefixParseFn func() tree.Expression
+	infixParseFn  func(tree.Expression) tree.Expression
+)
+
+// order of operations
+const (
+  _ int = iota
+  LOWEST
+  EQUALS
+  LESSGREATER
+  SUM
+  PRODUCT
+  PREFIX
+  CALL
+)
+
 type Parser struct {
 	tokens   []token.Token
 	token    token.Token
 	position int
 	Tree     *tree.Program
+
+  prefixParseFns map[token.TokenType]prefixParseFn
+  infixParseFns map[token.TokenType]infixParseFn
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+  p.prefixParseFns[tokenType] = fn
+}
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+  p.infixParseFns[tokenType] = fn
 }
 
 func Parse(tokens []token.Token) *tree.Program {
@@ -44,78 +71,90 @@ func (p *Parser) peekFor(t token.TokenType) bool {
 }
 
 func (p *Parser) ParseProgram() *tree.Program {
-  program := &tree.Program{}
-  statements := []tree.Statement{}
+	program := &tree.Program{}
+	statements := []tree.Statement{}
 
-  for p.token.Type != token.EOF {
-    statement := p.parseStatement()
-    p.nextToken()
-    statements = append(statements, statement)
-  }
+	for p.token.Type != token.EOF {
+		statement := p.parseStatement()
+		p.nextToken()
+		statements = append(statements, statement)
+	}
 
-  program.Statements = statements
-  return program
+	program.Statements = statements
+	return program
 }
-
 
 func (p *Parser) parseStatement() tree.Statement {
-  switch p.token.Type {
-  case token.LET:
-    return p.parseLetStatement()
-  case token.RETURN:
-    return p.parseReturnStatement()
-  default:
-    return p.parseExpressionStatement()
-  }
+	switch p.token.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
+	default:
+		return p.parseExpressionStatement()
+	}
 }
 
-
 func (p *Parser) parseLetStatement() *tree.LetStatement {
-  statement := &tree.LetStatement{Token: p.token}
+	statement := &tree.LetStatement{Token: p.token}
 
-  if !p.peekFor(token.IDENTIFIER) {
-    // TODO: handle these errors
-    return nil
-  }
-  p.nextToken()
+	if !p.peekFor(token.IDENTIFIER) {
+		// TODO: handle these errors
+		return nil
+	}
+	p.nextToken()
 
-  statement.Name = &tree.Identifier{Token: p.token, Value: p.token.Literal}
+	statement.Name = &tree.Identifier{Token: p.token, Value: p.token.Literal}
 
-  if !p.peekFor(token.ASSIGN) {
-    // TODO: make an error handler
-    return nil
-  }
-  p.nextToken()
+	if !p.peekFor(token.ASSIGN) {
+		// TODO: make an error handler
+		return nil
+	}
+	p.nextToken()
 
-  // TODO: parse the expression
-  // TODO: handle hitting EOF
-  for p.token.Type != token.SEMICOLON {
-    p.nextToken()
-  }
+	// TODO: parse the expression
+	// TODO: handle hitting EOF
+	for p.token.Type != token.SEMICOLON {
+		p.nextToken()
+	}
 
-  return statement
+	return statement
 }
 
 func (p *Parser) parseReturnStatement() *tree.ReturnStatement {
-  statement := &tree.ReturnStatement{Token: p.token}
+	statement := &tree.ReturnStatement{Token: p.token}
 
-  p.nextToken()
+	p.nextToken()
 
-  // again, we're skipping the expression for now
-  // it will need to be parsed correctly
-  for p.token.Type != token.SEMICOLON {
-    p.nextToken()
-  }
+	// again, we're skipping the expression for now
+	// it will need to be parsed correctly
+	for p.token.Type != token.SEMICOLON {
+		p.nextToken()
+	}
 
-  return statement
+	return statement
 }
 
 func (p *Parser) parseExpressionStatement() *tree.ExpressionStatement {
-  // TODO
-  return nil
+  statement := &tree.ExpressionStatement{Token: p.token}
+
+  statement.Expression = p.parseExpression(LOWEST)
+
+  if p.peekFor(token.SEMICOLON) {
+    p.nextToken()
+  }
+
+
+	return nil
 }
 
-func (p *Parser) parseExpression() tree.Expression {
-  // TODO
-  return nil
+func (p *Parser) parseExpression(precedence int) tree.Expression {
+  prefix := p.prefixParseFns[p.token.Type]
+  if prefix == nil {
+    // TODO: throw an error
+    return nil
+  }
+  leftExp := prefix()
+
+	return leftExp
 }
