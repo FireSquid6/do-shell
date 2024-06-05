0157d15daad5b0a2e7677c8f5aef21580e552aa8
firesquid6 <jdeiss06@gmail.com>
2024-06-04 19:49:13 -0500

    add different types of statements

--- parser/parser.go
+++ parser/parser.go
@@ -1,43 +1,48 @@
 package parser
 
 import (
-	"github.com/firesquid6/do-shell/lexer"
 	"github.com/firesquid6/do-shell/token"
 	"github.com/firesquid6/do-shell/tree"
 )
 
 type Parser struct {
-	l        *lexer.Lexer
+	tokens   []token.Token
 	token    token.Token
 	position int
+	Tree     *tree.Program
+}
+
+func Parse(tokens []token.Token) *tree.Program {
+	p := New(tokens)
+	return p.ParseProgram()
+
 }
 
 // the lexer should have already tokenized the text
-func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
-  p.position = 0
-  p.token = p.l.Tokens[p.position]
+func New(tokens []token.Token) *Parser {
+	p := &Parser{tokens: tokens}
+	p.position = 0
+	p.token = p.tokens[p.position]
 	return p
 }
 
 func (p *Parser) nextToken() {
-  p.position += 1
-  p.token = p.l.Tokens[p.position]
+	p.position += 1
+	p.token = p.tokens[p.position]
 }
 
 func (p *Parser) peek() token.Token {
-  return p.l.Tokens[p.position + 1]
+	return p.tokens[p.position+1]
 }
 
 func (p *Parser) peekFar(i int) token.Token {
-  return p.l.Tokens[p.position + i]
+	return p.tokens[p.position+i]
 }
 
 func (p *Parser) peekFor(t token.TokenType) bool {
-  return p.peek().Type == t
+	return p.peek().Type == t
 }
 
 func (p *Parser) ParseProgram() *tree.Program {
-  return nil
+	return nil
 }
-
--- tree/tree.go
+++ tree/tree.go
@@ -38,6 +38,56 @@ type LetStatement struct {
 	Name       *Identifier
 	Expression Expression
 }
-
-func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() []rune { return ls.Token.Literal }
+
+type FnStatement struct {
+  Token      token.Token
+  Name       *Identifier
+  Parameters []*Identifier
+  Body       []*Statement
+}
+func (s *FnStatement) statementNode() {}
+func (s *FnStatement) TokenLiteral() []rune { return s.Token.Literal }
+
+type ExpressionStatement struct {
+  Token      token.Token
+  Expression Expression
+}
+func (s *ExpressionStatement) statementNode() {}
+func (s *ExpressionStatement) TokenLiteral() []rune { return s.Token.Literal }
+
+type ReturnStatement struct {
+  Token      token.Token
+  ReturnValue Expression
+}
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() []rune { return rs.Token.Literal }
+
+type IfStatement struct {
+  Token       token.Token
+  Expression Expression
+  Body []*Statement
+}
+func (is *IfStatement) statementNode() {}
+func (is *IfStatement) TokenLiteral() []rune { return is.Token.Literal }
+
+type ElifStatement struct {
+  Token       token.Token
+  Expression Expression
+  Body []*Statement
+}
+func (es *ElifStatement) statementNode() {}
+func (es *ElifStatement) TokenLiteral() []rune { return es.Token.Literal }
+
+type ElseStatement struct {
+  Token token.Token
+  Body []*Statement
+}
+func (es *ElseStatement) statementNode() {}
+func (es *ElseStatement) TokenLiteral() []rune { return es.Token.Literal }
+
+
+
+
+
