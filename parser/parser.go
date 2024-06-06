bbda65f348ac4870a29749993b6dccd051902921
firesquid6 <jdeiss06@gmail.com>
2024-06-05 22:22:47 -0500

    add the beginnings of prefix parsing

--- parser/parser.go
+++ parser/parser.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"github.com/firesquid6/do-shell/token"
 	"github.com/firesquid6/do-shell/tree"
+	"strconv"
 )
 
 type (
@@ -12,14 +13,14 @@ type (
 
 // order of operations
 const (
-  _ int = iota
-  LOWEST
-  EQUALS
-  LESSGREATER
-  SUM
-  PRODUCT
-  PREFIX
-  CALL
+	_ int = iota
+	LOWEST
+	EQUALS
+	LESSGREATER
+	SUM
+	PRODUCT
+	PREFIX
+	CALL
 )
 
 type Parser struct {
@@ -28,15 +29,15 @@ type Parser struct {
 	position int
 	Tree     *tree.Program
 
-  prefixParseFns map[token.TokenType]prefixParseFn
-  infixParseFns map[token.TokenType]infixParseFn
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
-  p.prefixParseFns[tokenType] = fn
+	p.prefixParseFns[tokenType] = fn
 }
 func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
-  p.infixParseFns[tokenType] = fn
+	p.infixParseFns[tokenType] = fn
 }
 
 func Parse(tokens []token.Token) *tree.Program {
@@ -50,7 +51,31 @@ func New(tokens []token.Token) *Parser {
 	p := &Parser{tokens: tokens}
 	p.position = 0
 	p.token = p.tokens[p.position]
+
+	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+	p.infixParseFns = make(map[token.TokenType]infixParseFn)
+
+	p.registerPrefix(token.IDENTIFIER, p.parseIdentifier)
+	p.registerPrefix(token.NUMBER, p.parseNumberLiteral)
+
 	return p
+}
+
+func (p *Parser) parseIdentifier() tree.Expression {
+	return &tree.Identifier{Token: p.token, Value: p.token.Literal}
+}
+
+func (p *Parser) parseNumberLiteral() tree.Expression {
+	// TODO: handle it being a float and not an integer
+	literal := &tree.IntegerLiteral{Token: p.token}
+	value, err := strconv.ParseInt(string(p.token.Literal), 0, 64)
+
+	if err != nil {
+		// todo: handle this error
+	}
+
+	literal.Value = value
+	return literal
 }
 
 func (p *Parser) nextToken() {
@@ -136,25 +161,24 @@ func (p *Parser) parseReturnStatement() *tree.ReturnStatement {
 }
 
 func (p *Parser) parseExpressionStatement() *tree.ExpressionStatement {
-  statement := &tree.ExpressionStatement{Token: p.token}
+	statement := &tree.ExpressionStatement{Token: p.token}
 
-  statement.Expression = p.parseExpression(LOWEST)
+	statement.Expression = p.parseExpression(LOWEST)
 
-  if p.peekFor(token.SEMICOLON) {
-    p.nextToken()
-  }
-
+	if p.peekFor(token.SEMICOLON) {
+		p.nextToken()
+	}
 
 	return nil
 }
 
 func (p *Parser) parseExpression(precedence int) tree.Expression {
-  prefix := p.prefixParseFns[p.token.Type]
-  if prefix == nil {
-    // TODO: throw an error
-    return nil
-  }
-  leftExp := prefix()
+	prefix := p.prefixParseFns[p.token.Type]
+	if prefix == nil {
+		// TODO: throw an error
+		return nil
+	}
+	leftExp := prefix()
 
 	return leftExp
 }
--- tree/tree.go
+++ tree/tree.go
@@ -4,7 +4,7 @@ import "github.com/firesquid6/do-shell/token"
 
 type Node interface {
 	TokenLiteral() []rune
-  String() string
+	String() string
 }
 
 type Statement interface {
@@ -20,19 +20,47 @@ type Expression interface {
 type Program struct {
 	Statements []Statement
 }
+
 func (p *Program) String() string {
-  var out string
+	var out string
 
-  for _, s := range p.Statements {
-    out += s.String()
-  }
+	for _, s := range p.Statements {
+		out += s.String()
+	}
 
-  return out
+	return out
 }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() []rune { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return string(il.Token.Literal) }
 
 type Identifier struct {
 	Token token.Token
 	Value []rune
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() []rune { return i.Token.Literal }
+func (i *Identifier) String() string       { return string(i.Value) }
+
+// TODO: string and command expressions
+
+type PrefixExpression struct {
+	Token    token.Token
+	Operator string
+	Right    Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() []rune { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string {
+	return "(" + pe.Operator + pe.Right.String() + ")"
 }
 
 func (p *Program) TokenLiteral() []rune {
@@ -48,34 +76,33 @@ type LetStatement struct {
 	Name       *Identifier
 	Expression Expression
 }
-func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() []rune { return ls.Token.Literal }
 func (ls *LetStatement) String() string {
-  return string(ls.TokenLiteral()) + " " + string(ls.Name.Value) + " = " + ls.Expression.String() + ";"
+	return string(ls.TokenLiteral()) + " " + string(ls.Name.Value) + " = " + ls.Expression.String() + ";"
 }
 
 type ReturnStatement struct {
-  Token      token.Token
-  ReturnValue Expression
+	Token       token.Token
+	ReturnValue Expression
 }
-func (rs *ReturnStatement) statementNode() {}
+
+func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() []rune { return rs.Token.Literal }
 func (rs *ReturnStatement) String() string {
-  return string(rs.TokenLiteral()) + " " + rs.ReturnValue.String() + ";"
+	return string(rs.TokenLiteral()) + " " + rs.ReturnValue.String() + ";"
 }
 
 type ExpressionStatement struct {
-  Token      token.Token
-  Expression Expression
+	Token      token.Token
+	Expression Expression
 }
-func (s *ExpressionStatement) statementNode() {}
+
+func (s *ExpressionStatement) statementNode()       {}
 func (s *ExpressionStatement) TokenLiteral() []rune { return s.Token.Literal }
 func (s *ExpressionStatement) String() string {
-  return s.Expression.String()
+	return s.Expression.String()
 }
 
 // TODO: if, for, while, else, elif statements
-
-
-
-
