e41c5bfaf5f1db846f1466e1ee62cad7c17ffa27
firesquid6 <jdeiss06@gmail.com>
2024-06-08 15:09:11 -0500

    format code better

--- option/option.go
+++ option/option.go
@@ -3,46 +3,44 @@ package option
 
 import "fmt"
 
-
 type Option[T any] struct {
-  value T
-  isFull bool
+	value  T
+	isFull bool
 }
 
-
 func Some[T any](value T) Option[T] {
-  return Option[T]{value: value, isFull: true}
+	return Option[T]{value: value, isFull: true}
 }
 
 func None[T any]() Option[T] {
-  return Option[T]{isFull: false}
+	return Option[T]{isFull: false}
 }
 
 func (s *Option[T]) Get() T {
-  if !s.isFull {
-    panic("Option is empty")
-  }
-  return s.value
+	if !s.isFull {
+		panic("Option is empty")
+	}
+	return s.value
 }
 
 func (s *Option[T]) GetOrElse(defaultValue T) (T, bool) {
-  if !s.isFull {
-    return defaultValue, true
-  }
-  return s.value, false
+	if !s.isFull {
+		return defaultValue, true
+	}
+	return s.value, false
 }
 
 func (s *Option[T]) IsFull() bool {
-  return s.isFull
+	return s.isFull
 }
 
 func (s *Option[_]) IsEmpty() bool {
-  return !s.isFull
+	return !s.isFull
 }
 
 func (s *Option[T]) String() string {
-  if !s.isFull {
-    return "None"
-  }
-  return fmt.Sprintf("Some(%v)", s.value)
+	if !s.isFull {
+		return "None"
+	}
+	return fmt.Sprintf("Some(%v)", s.value)
 }
