99331a2ae7468346431c6d1953172546f132e5be
firesquid6 <jdeiss06@gmail.com>
2024-09-22 15:55:20 -0500

    reset project

--- interpreter/interpreter.go
+++
@@ -1,8 +0,0 @@
-package interpreter
-
-// TODO - finish everything that needs to be inside here
-type InterpreterContext struct {
-	Numbers map[string]float64
-	Strings map[string]string
-	Errors  []error
-}
