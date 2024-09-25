36103729975ff59ffe09a83dcdd0259b07b2359e
firesquid6 <jdeiss06@gmail.com>
2024-09-24 21:40:21 -0500

    add scanner module

--- src/interpreter/scanner.rs
+++ src/interpreter/scanner.rs
@@ -3,3 +3,15 @@
 pub struct Scanner {
 
 }
+
+
+fn new_scanner() -> Scanner {
+    Scanner {}
+}
+
+
+#[cfg(test)]
+fn test_scanner() {
+    let scanner = new_scanner();
+
+}
--- src/language_server/mod.rs
+++ src/language_server/mod.rs
@@ -1,3 +1,3 @@
-pub fn start_server(root: Option<String>) {
+pub fn start_server(_: Option<String>) {
     println!("Starting language server...");
 }
