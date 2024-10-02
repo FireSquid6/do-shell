082a2947e82384def246ce5988dbace5230373f7
firesquid6 <jdeiss06@gmail.com>
2024-10-02 17:59:48 -0500

    delete uneccessary stuff

--- tools/generate-ast.ts
+++ tools/generate-ast.ts
@@ -1,1 +1,23 @@
-// metaprogramming
+// metaprogramming the abstract syntax tree
+// this is hacky and bad, but that is ok!
+import fs from "fs";
+
+const outputFile = "src/interpreter/ast.rs"
+const types = [
+  "Binary:Expr left,Token operator,Expr right",
+  "Grouping:Expr expression",
+  "Literal:String value,Token token",
+  "Unary:Token operator,Expr right",
+]
+
+const str = `
+use super::error::{InterpreterErrors, ErrorKind};
+use super::scanner::*;
+
+struct Expr
+`
+
+
+
+
+
