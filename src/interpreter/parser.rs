5d898d6d23cc23da5ec8ff002ffb1b4fd727acb5
firesquid6 <jdeiss06@gmail.com>
2024-10-14 09:33:52 -0500

    add interpreter errors

--- src/interpreter/parser.rs
+++ src/interpreter/parser.rs
@@ -1,10 +1,12 @@
 use super::scanner::*;
 use super::tree::*;
+use super::error::*;
 
 type Program = Vec<Box<dyn Statement>>;
+
 fn parse_into_tree(tokens: &Vec<Token>) -> Program {
     let program: Program = Vec::new();
-
+    let errors: Vec<InterpreterError> = Vec::new();
 
     return program;
 
